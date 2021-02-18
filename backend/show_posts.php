0ff29fc7de872dae0ec1bd053531632cba9085ec
jeadelso <jeadelso@iu.edu>
2021-02-18 15:05:37 -0600

    Settings functionality

---
+++ backend/show_posts.php
@@ -0,0 +1,32 @@
+<?php
+
+// include_once('./session.php');
+// session_start();
+include_once("./connection.php");
+
+
+$incoming_data = json_decode(file_get_contents('php://input'), true);
+
+// $session_id = $incoming_data['session_id'];
+$session_id = '1234567';
+if ($session_id != null) {
+    $sql = "SELECT * FROM posts";
+
+    if ($result = mysqli_query($conn, $sql)) {
+
+        if (mysqli_num_rows($result) > 0) {
+
+            $response_header["posts"] =
+                mysqli_fetch_all($result, MYSQLI_ASSOC);
+            echo json_encode($response_header);
+        } else {
+            $response_header["message"] = " No Post found";
+            echo json_encode($response_header);
+        }
+        // echo json_encode($response_header);
+    } else {
+        $response_header['session_id'] = $session_id;
+        $response_header["message"] = " Please login again";
+        echo json_encode($response_header);
+    }
+}
