ae63f9347a5d575f688d08f43aeb3f1a8eedf695
jeadelso <jeadelso@iu.edu>
2021-02-18 17:18:26 -0600

    update

--- backend/signup.php
+++ backend/signup.php
@@ -41,7 +41,7 @@ if (!$conn) {
             $password_encription
                 = password_hash($form_data[3], PASSWORD_DEFAULT);
             $$userID = rand(pow(10, $digits - 1), pow(10, $digits) - 1);
-              $sql_insert = "INSERT INTO Profile (`ProfileID`, `firstName`, `lastName`, `userName`, `password`, `followers` , `bio` ) VALUES ( '$userID', '$form_data[1]', '$form_data[2]' , '$form_data[0]' , '$form_data[3]' , ''  , ''
+             $sql_insert = "INSERT INTO Profile (`ProfileID`, `firstName`, `lastName`, `userName`, `password`, `followers` , `bio` ) VALUES ( '599', '$form_data[1]', '$form_data[2]' , '$form_data[0]' , '$form_data[3]' , '' , '')";
 
             if (mysqli_query($conn, $sql_insert)) {
 
