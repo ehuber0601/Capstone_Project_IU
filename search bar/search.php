bcc278a2f4c1d6926c55b70af45e91a93a2f162b
Nagy, Allison L <allnagy@indiana.edu>
2021-02-22 12:15:22 -0500

    Update search.php
    looked at old i308 files to see if they could help me with SQL statements and tried a new way of writing query

--- search bar/search.php
+++ search bar/search.php
@@ -1,62 +1,56 @@
 <?php
+// Create connection
+$conn=mysqli_connect("db.luddy.indiana.edu","i494f20_allnagy","my+sql=i494f20_allnagy","i494f20_allnagy");
 
-// setting header file to accept request
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error() . "<br>");
+}
 
+// Grab POST Data
+$var_search = mysqli_real_escape_string($conn, $_GET['searchbar']);
 
-	mysql_connect("db.luddy.indiana.edu", "i494f20_allnagy", "my+sql=i494f20_allnagy") or die("Error connecting to database: ".mysql_error());
-	
-	mysql_select_db("i494f20_allnagy") or die(mysql_error());
-	/* tutorial_search is the name of database we've created */
+// Create SQL Statement
+$sql = ("SELECT * FROM Song WHERE (`title` LIKE '%".$var_search."%') OR (`artistName` LIKE '%".$var_search."%') OR (`genre` LIKE '%".$var_search."%')");
+
+// Get Results
+$result = mysqli_query($conn, $sql);
+
+// Get Number of Rows
+$num_rows = mysqli_num_rows($result);
+
+// Display Results
+if ($result->num_rows > 0) {
+    echo "<table>";
+	echo "<tr>
+	<th>Shift ID</th>
+	<th>Employee ID</th>
+	<th>wdate</th>
+	<th>time_in</th>
+	<th>time_out</th>
+	<th>role</th>
+	</tr>";
+    // Output data of each row, ->fetch_assoc gives array of arrays with keys matching column names
+    //while($row = $result->fetch_assoc()) {
+        //echo "<tr><td>".$row["shiftid"]."</td><td>".$row["empid"]."</td><td>".$row["wdate"]."</td>
+		      //<td>".$row["time_in"]."</td><td>".$row["time_out"]."</td><td>".$row["role"]."</td></tr>";
+		      
+		      
+while ($row = mysql_fetch_array($result)){ 
+echo 'songID: ' .$row['songID']; 
+echo '<br /> artistID: ' .$row['artistID']; 
+echo '<br /> Artist Name: '.$row['artistName']; 
+echo '<br /> Time: '.$row['length']; 
+echo '<br /> Genre: '.$row['genre'];  
+echo '<br /> Title: '.$row['title'];
+} 
+    }
+    echo "</table>";
+} else {
+    echo "0 results <br>";
+}
+echo "$num_rows Rows <br>";
+
+// Close Connection
+mysqli_close($conn);
 ?>
-<!DOCTYPE html>
-<head>
-	<title>Search results</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" type="text/css" href="style.css"/>
-</head>
-<body>
-<?php
-	$query = $_GET['query']; 
-	// gets value sent over search form
-	
-	$min_length = 3;
-	
-	if(strlen($query) >= $min_length){ // if query length is more or equal minimum length then
-		
-		$query = htmlspecialchars($query); 
-		// changes characters used in html to their equivalents, for example: < to &gt;
-		
-		$query = mysql_real_escape_string($query);
-		// makes sure nobody uses SQL injection
-		
-		$raw_results = mysql_query("SELECT * FROM Song
-			WHERE (`title` LIKE '%".$query."%') OR (`artistName` LIKE '%".$query."%') OR (`genre` LIKE '%".$query."%')") or die(mysql_error());
-			
-		// * means that it selects all fields, you can also write: `title`, `artistName`, `genre`
-		// Song is the name of our table
-		
-		// '%$query%' is what we're looking for, % means anything, for example if $query is Hello
-		// it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
-		// or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
-		
-		if(mysql_num_rows($raw_results) > 0){ // if one or more rows are returned do following
-			
-			while($results = mysql_fetch_array($raw_results)){
-			// $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
-			
-				echo "<p><h3>".$results['title']."</h3>".$results['text']."</p>";
-				// posts results gotten from database(title and text) you can also show id ($results['id'])
-			}
-			
-		}
-		else{ // if there is no matching rows do following
-			echo "No results";
-		}
-		
-	}
-	else{ // if query length is less than minimum
-		echo "Minimum length is ".$min_length;
-	}
-?>
-</body>
-</html>
