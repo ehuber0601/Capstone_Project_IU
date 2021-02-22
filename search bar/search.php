e6472e90db94c3b9a89081e2d79944bdf883d8be
Nagy, Allison L <allnagy@indiana.edu>
2021-02-22 13:17:27 -0500

    Update search.php
    table is arranged in order of how data should be displayed, next step will be to use CSS to match the style of the rest of the application

--- search bar/search.php
+++ search bar/search.php
@@ -6,17 +6,17 @@ $conn=mysqli_connect("db.luddy.indiana.edu","i494f20_allnagy","my+sql=i494f20_al
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error() . "<br>");
 }
-        
+
 // Grab POST Data
 $var_search = mysqli_real_escape_string($conn, $_GET['searchbar']);
-        
-// Create SQL Statement 
-$sql = ("SELECT * FROM Song WHERE (`title` LIKE '%".$var_search."%') OR (`artistName` LIKE '%".$var_search."%') OR (`genre` LIKE
+
+// Create SQL Statement
+$sql = ("SELECT * FROM Song WHERE (`title` LIKE '%".$var_search."%') OR (`artistName` LIKE '%".$var_search."%') OR (`genre` L$
 '%".$var_search."%')");
-        
+
 // Get Results
 $result = mysqli_query($conn, $sql);
-        
+
 // Get Number of Rows
 $num_rows = mysqli_num_rows($result);
 
@@ -25,18 +25,17 @@ echo $_POST['searchname'];
 if ($result->num_rows > 0) {
     echo "<table>";
         echo "<tr>
-        <th>Song ID</th>
         <th>Artist</th>
-        <th>Time</th>
+        <th>Song</th>
+        <th>Length</th>
         <th>Genre</th>
-        <th>Title</th>
         </tr>";
     // Output data of each row, ->fetch_assoc gives array of arrays with keys matching column names
-	while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["songID"]."</td><td>".$row["artistName"]."</td><td>".$row["length"]."</td>
-        	<td>".$row["genre"]."</td><td>".$row["title"]."</td></tr>";
+        while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["artistName"]."</td><td>".$row["title"]."</td><td>".$row["length"]."</td>
+                <td>".$row["genre"]."</td></tr>";
 
-}    
+}
     }
     echo "</table>";
 echo "$num_rows Rows <br>";
