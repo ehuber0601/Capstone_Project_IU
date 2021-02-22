a09b9c68f750985bede1e79073487e53e04d7a22
Nagy, Allison L <allnagy@indiana.edu>
2021-02-22 13:08:29 -0500

    Update searchbar.php
    functional search bar with searchbar.php page

--- search bar/searchbar.php
+++ search bar/searchbar.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <head>
-	<title>Search Bar</title>
-	<meta charset=utf-8" />
-	<link rel="stylesheet" href="style.css"/>
+        <title>Search Bar</title>
+        <meta charset=utf-8" />
+        <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-	<p> Search for a song or artist </p>
-	<form method="POST" action="search.php">
-		<input type="text" name="searchbar" />
-		<input type="submit" value="Search" />
-	</form>
+        <p> Search for a song or artist </p>
+        <form method="POST" action="search.php">
+                <input type="text" name="searchbar" />
+                <input type="submit" value="Search" />
+        </form>
 </body>
 </html>
