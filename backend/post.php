56be1b533c5fdfac0e81eec2f2f4f588d2f5ab87
jeadelso <jeadelso@iu.edu>
2021-02-25 10:17:40 -0500

    Added Buttons to index html and post-form.html

---
+++ backend/post.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+  ...
+  </head>
+  <body>
+    ...
+    <form action="post.php" method="POST">
+      <label>Name: <input type="text" name="name" /><label>
+      <input type="Submit" value="Submit" />
+    </form>
+    
+    ...
+  </body>
+</html>
