a9e36a57626d4d3ac29fe35803fc41de9da549d1
jeadelso <jeadelso@iu.edu>
2021-03-01 12:41:20 -0500

    php file test

---
+++ backend/post.php
@@ -0,0 +1,20 @@
+<?php
+
+// include_once("./session.php");
+include_once("./connection.php");
+header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With,Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT');
+
+session_start();
+
+
+$incoming_data = json_decode(file_get_contents('php://input'), true);
+
+$postbox = $incoming_data['postbox'];
+
+
+
+$sql = " SELECT * FROM `Profile` limit 5;"
+echo json_encode($postbox);
