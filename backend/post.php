1b94079f93e1e67b6041242acb13caacb4347122
jeadelso <jeadelso@iu.edu>
2021-03-01 20:32:36 -0500

    repose.parse()

--- backend/post.php
+++ backend/post.php
@@ -6,13 +6,14 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With,Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT');
 session_start();
+$incoming_data = json_decode(file_get_contents('php://input'), true);
+
+$postbox = $incoming_data['postbox'];
 ?>
 
 
 
-// $incoming_data = json_decode(file_get_contents('php://input'), true);
 
-// $postbox = $incoming_data['postbox'];
 
 
 
