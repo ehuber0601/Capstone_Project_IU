837f9a33d4735de7c36f010901b3a3d2f7fe7d4b
jeadelso <jeadelso@iu.edu>
2021-03-01 20:18:03 -0500

    change

--- backend/post.php
+++ backend/post.php
@@ -1,14 +1,13 @@
 <?php
-
+include_once("./session.php");
+include_once("./connection.php");
+header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With,Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT');
+session_start();
 ?>
-// include_once("./session.php");
-// include_once("./connection.php");
-// header("Content-Type: application/json");
-// header('Access-Control-Allow-Origin: *');
-// header("Access-Control-Allow-Headers: Origin, X-Requested-With,Content-Type, Accept");
-// header('Access-Control-Allow-Methods: GET, POST, PUT');
 
-// session_start();
 
 
 // $incoming_data = json_decode(file_get_contents('php://input'), true);
