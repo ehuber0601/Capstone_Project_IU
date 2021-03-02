1eeaaadbaf3591597e41deb298ff88723d21caf8
jeadelso <jeadelso@iu.edu>
2021-03-01 19:29:35 -0500

    edit post.php

--- backend/post.php
+++ backend/post.php
@@ -1,21 +1,21 @@
 <?php
 echo "hello world"
 
-include_once("./session.php");
-include_once("./connection.php");
-header("Content-Type: application/json");
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With,Content-Type, Accept");
-header('Access-Control-Allow-Methods: GET, POST, PUT');
+// include_once("./session.php");
+// include_once("./connection.php");
+// header("Content-Type: application/json");
+// header('Access-Control-Allow-Origin: *');
+// header("Access-Control-Allow-Headers: Origin, X-Requested-With,Content-Type, Accept");
+// header('Access-Control-Allow-Methods: GET, POST, PUT');
 
-session_start();
-
-
-$incoming_data = json_decode(file_get_contents('php://input'), true);
-
-$postbox = $incoming_data['postbox'];
+// session_start();
 
 
+// $incoming_data = json_decode(file_get_contents('php://input'), true);
 
-$sql = " SELECT * FROM `Profile` limit 5;"
-echo json_encode($postbox);
+// $postbox = $incoming_data['postbox'];
+
+
+
+// $sql = " SELECT * FROM `Profile` limit 5;"
+// echo json_encode($postbox);
