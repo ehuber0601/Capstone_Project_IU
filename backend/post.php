d0a63b7f1816e0167b5d31aa0f23e3393821a3ee
jeadelso <jeadelso@iu.edu>
2021-03-01 20:35:30 -0500

    repose.parse()

--- backend/post.php
+++ backend/post.php
@@ -10,7 +10,6 @@ $incoming_data = json_decode(file_get_contents('php://input'), true);
 
 $postbox = $incoming_data['postbox'];
 $sql = " SELECT * FROM `Profile` limit 5;"
-echo json_encode($postbox);
 ?>
 
 
