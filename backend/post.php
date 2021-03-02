a551971aa80f732e2708c3a076bdec8dadd8d8a7
jeadelso <jeadelso@iu.edu>
2021-03-02 14:17:51 -0500

    test

--- backend/post.php
+++ backend/post.php
@@ -5,12 +5,6 @@ header("Content-Type: application/json");
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With,Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT');
-function console_log($output, $with_script_tags = true) {
-    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .');';
-    if ($with_script_tags) {
-        $js_code = '<script>' . $js_code . '</script>';
-    }
-    echo $js_code;
 session_start();
 $incoming_data = json_decode(file_get_contents('php://input'), true);
 
