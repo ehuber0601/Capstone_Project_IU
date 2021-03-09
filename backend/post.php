6257536837a2a8ab88558348eb41220fcb9cbe35
jeadelso <jeadelso@iu.edu>
2021-03-08 23:59:18 -0500

    changes to post.php

--- backend/post.php
+++ backend/post.php
@@ -9,12 +9,17 @@ session_start();
 $incoming_data = json_decode(file_get_contents('php://input'), true);
 
 $postbox = $incoming_data['postbox'];
-$dummy_post_id=12421;
-$dummy_user_id=124921;
-$dummy_post_date="2/16/1999";
+// $dummy_post_id = 12421;
+$dummy_post_id = rand(11, 1000);
+$dummy_user_id = 124921;
+$dummy_post_date = "2/16/1999";
 $dummy_likes = 0;
 
-$sql_insert = "INSERT INTO Posts ( `postID`, `userID`, `postContent`, `postDate`, `likes`  ) VALUES (  '$dummy_post_id', '$dummy_user_id' , '$postbox' , '$dummy_post_date' , '$dummy_likes' )";
+$incoming_data = json_decode(file_get_contents('php://input'), true);
+
+$user_id = (int)$incoming_data['userID'];
+// postID, userID, postContent, postDate, likes
+$sql_insert = "INSERT INTO Posts ( `postID`, `userID`, `postContent`, `postDate`, `likes`  ) VALUES ('$dummy_post_id', '$user_id' , '$postbox' , '$dummy_post_date' , '$dummy_likes' )";
 if (mysqli_query($conn, $sql_insert)) {
     $response_header['status_code'] = 200;
     $response_header['response_message'] = 'Post Added Successfully';
@@ -25,9 +30,6 @@ if (mysqli_query($conn, $sql_insert)) {
     $response_header['response_message'] = 'Error' . mysqli_error($conn);
     echo json_encode($response_header);
 }
-
-
-?>
 
 
 
