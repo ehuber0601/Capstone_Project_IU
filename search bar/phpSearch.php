e59721618d1b5bb471ea73b397bca6ce5d9163f0
Nagy, Allison L <allnagy@indiana.edu>
2021-03-10 20:15:29 -0500

    Update phpSearch.php

--- search bar/phpSearch.php
+++ search bar/phpSearch.php
@@ -13,7 +13,7 @@ if ($conn->connect_error){
 	die("Connection failed: ". $conn->connect_error);
 }
 
-$sql = "select * from Song where name like '%$search%'";
+$sql = "select * from Song where title like '%$search%'";
 
 $result = $conn->query($sql);
 
