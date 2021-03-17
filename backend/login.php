6f1be45502a052962f171a7110d2549ad25a8a7a
jeadelso <jeadelso@iu.edu>
2021-03-17 16:55:31 -0500

    login.php

--- backend/login.php
+++ backend/login.php
@@ -15,31 +15,43 @@ $name = $incoming_data['username'];
 $userPassword = $incoming_data['password'];
 
 
-$sql = " SELECT * FROM `Profile` WHERE `username` = '$name' AND `password` = '$userPassword'";
+$sql = " SELECT * FROM `User` WHERE `username` = '$name'";
 
 if ($result = mysqli_query($conn, $sql)) {
     if (mysqli_num_rows($result) <= 0) {
         $row = mysqli_fetch_assoc($result);
         // $response_header['username'] = $row['username'];
 
-        $response_header['response_message'] = "Sorry Invalid credentials ";
+        $response_header['response_message'] = "Sorry No user exist with email or username " . $name . " exists";
         $response_header['result_code'] = 206;
         echo json_encode($response_header);
     } else {
-        $row = mysqli_fetch_assoc($result);
-        // session_start();
-        $_SESSION['username'] = $name;
-        $session_id = generateRandomString(12);
-        $_SESSION['session_id'] = $session_id;
-        // $response_header['session_id2'] = $_SESSION['session_id'];
-        $response_header['username'] = $row['username'];
-        $response_header['session_id'] = $_SESSION['session_id'];
-        $response_header['userID'] = $row['userID'];
-        $response_header['result_code'] = 200;
-        $response_header['response_message'] = "Success";
-        echo json_encode($response_header);
+
+        $sql = " SELECT * FROM `User` WHERE `username` = '$name' AND `password` = '$userPassword'";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) >= 1) {
+
+            $row = mysqli_fetch_assoc($result);
+            // session_start();
+            $_SESSION['username'] = $name;
+            $session_id = generateRandomString(12);
+            $_SESSION['session_id'] = $session_id;
+            // $response_header['session_id2'] = $_SESSION['session_id'];
+            $response_header['username'] = $row['username'];
+            $response_header['session_id'] = $_SESSION['session_id'];
+            $response_header['userID'] = $row['userID'];
+            $response_header['result_code'] = 200;
+            $response_header['response_message'] = "Success";
+            echo json_encode($response_header);
+        } else {
+
+            $response_header['response_message'] = "Sorry You gave entered invalid password ! try another password";
+            $response_header['result_code'] = 206;
+            echo json_encode($response_header);
+        }
     }
 } else {
-    $response_header['response_message'] = "Error :" . mysqli_error($conn);
+    $response_header['response_message'] = "Error : " . mysqli_error($conn);
     echo json_encode($response_header);
 }
