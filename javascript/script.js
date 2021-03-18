b9ed799c4c04383620c1dfe76caa1dc2b2819333
jeadelso <jeadelso@iu.edu>
2021-03-18 16:51:07 -0500

    show posts to homepage

--- javascript/script.js
+++ javascript/script.js
@@ -57,4 +57,140 @@ function login() {
     );
 }
 
+function load_posts() {
+  // console.log("Sessionid is", localStorage.getItem("session_id"));
+  var json = { session_id: localStorage.getItem("session_id") };
+  fetch(this.base_url + "show_posts.php", {
+    method: "POST",
+    mode: "no-cors",
+    body: JSON.stringify(json),
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.log("response is", response);
+        throw new Error("Could not reach website.");
+      }
+      console.log(response);
+      return response.json();
+    })
+    .then(function (json) {
+      console.log("Data from fetch");
+      console.log(json["posts"]);
+      var myarray = json["posts"];
+      myarray.forEach(function (item, index) {
+        // console.log("likes is", item.likes);
+        // console.log("Post Content", item.postContent);
+        // console.log("Date", item.postDate);
+
+        document.getElementById(
+          "post-content"
+        ).innerHTML += `<div class="post p-2 border mt-2 mb-2">
+                <P class="top-heading mb-2">Posted-by <span></span></P>
+                <p class="top-heading mb-2">Post Content</p>
+                <p class="mt-2 mb-2">${item.postContent}</p>
+                <p class="mt-2 mb-2 top-heading"> Posted on <span> ${
+                  item.postDate
+                } </span></p>
+
+                <div class="d-flex justify-content-between mt-2 mb-2">
+                    <p onclick="update_likes(${
+                      item.postID
+                    })">Like <i class="fa fa-thumbs-o-up" aria-hidden="true"></i> <span> ${
+          item.likes
+        }</span> </p>
+                    <p>comments <i class="fa fa-comment" aria-hidden="true"></i> <span></span> </p>
+                    <p>Total Views <span>${item.likes * 2}</span> </p>
+                </div>
+            </div>`;
+        // Object.entries(item).forEach(([key, value]) =>
+        //   console.log(`${key}: ${value}`)
+        // );
+      });
+
+      //   document.getElementById("response-message").innerHTML =
+      //     json["response_message"];
+
+      if (json["result_code"] === 200) {
+        console.log("hi");
+        // window.location = "./index.html";
+      } else if (json["result_code"] === 206) {
+        console.log("206");
+        // window.location = "./index.html";
+      }
+    })
+    .catch((err) => console.error(err));
+}
+
+
+
+function load_posts() {
+  // console.log("Sessionid is", localStorage.getItem("session_id"));
+  var json = { session_id: localStorage.getItem("session_id") };
+  fetch(this.base_url + "show_posts.php", {
+    method: "POST",
+    mode: "no-cors",
+    body: JSON.stringify(json),
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.log("response is", response);
+        throw new Error("Could not reach website.");
+      }
+      console.log(response);
+      return response.json();
+    })
+    .then(function (json) {
+      console.log("Data from fetch");
+      console.log(json["posts"]);
+      var myarray = json["posts"];
+      myarray.forEach(function (item, index) {
+        // console.log("likes is", item.likes);
+        // console.log("Post Content", item.postContent);
+        // console.log("Date", item.postDate);
+
+        document.getElementById(
+          "post-content"
+        ).innerHTML += `<div class="post p-2 border mt-2 mb-2">
+                <P class="top-heading mb-2">Posted-by <span></span></P>
+                <p class="top-heading mb-2">Post Content</p>
+                <p class="mt-2 mb-2">${item.postContent}</p>
+                <p class="mt-2 mb-2 top-heading"> Posted on <span> ${
+                  item.postDate
+                } </span></p>
+
+                <div class="d-flex justify-content-between mt-2 mb-2">
+                    <p onclick="update_likes(${
+                      item.postID
+                    })">Like <i class="fa fa-thumbs-o-up" aria-hidden="true"></i> <span> ${
+          item.likes
+        }</span> </p>
+                    <p>comments <i class="fa fa-comment" aria-hidden="true"></i> <span></span> </p>
+                    <p>Total Views <span>${item.likes * 2}</span> </p>
+                </div>
+            </div>`;
+        // Object.entries(item).forEach(([key, value]) =>
+        //   console.log(`${key}: ${value}`)
+        // );
+      });
+
+      //   document.getElementById("response-message").innerHTML =
+      //     json["response_message"];
+
+      if (json["result_code"] === 200) {
+        console.log("hi");
+        // window.location = "./index.html";
+      } else if (json["result_code"] === 206) {
+        console.log("206");
+        // window.location = "./index.html";
+      }
+    })
+    .catch((err) => console.error(err));
+}
+
 
