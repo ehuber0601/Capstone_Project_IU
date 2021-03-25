014f6382f2b6cc559fd5edd5071eccb625abd057
Nagy, Allison L <allnagy@indiana.edu>
2021-03-25 16:51:50 -0400

    Update quizInsert.php
    trying to see if saving each item from the form as a variable will allow me to insert the values into the database

--- quiz/quizInsert.php
+++ quiz/quizInsert.php
@@ -13,13 +13,19 @@ if ($conn->connect_error){
 	die("Connection failed: ". $conn->connect_error);
 }
 
-$sql = "INSERT INTO Quiz ('username','color','day','season','vacation','food')  
-VALUES ('".$_POST["username"]."','".$_POST["color"]."','".$_POST["day"]."',
-'".$_POST["season"]."','".$_POST["vacation"]."','".$_POST["food"]."')";
+$username = $_POST['username'];
+$color = $_POST['color'];
+$day = $_POST['day'];
+$season = $_POST['season'];
+$vacation = $_POST['vacation'];
+$food = $_POST['food'];
+
+$sql = "INSERT INTO `Quiz` (`username`, `color`, `day`, `season`, `vacation`,'food') 
+VALUES ('$username', '$color', '$day', '$season', '$vacation', '$food');"
 
 $select = "select * from Quiz";
 
-$result = $conn->query($sql);
+$result = $conn->query($select);
 
 if ($result->num_rows > 0){
 while($row = $result->fetch_assoc() ){
