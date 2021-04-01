ea746921e432dc0e519456b5697a57de9df8a54a
ehuberm <ehuberm@iu.edu>
2021-04-01 14:50:17 -0400

    Progress
    Live event

---
+++ live_event.php
@@ -0,0 +1,21 @@
+<?
+$servername='db.luddy.indiana.edu';
+        	$username= 'i494f20_team12';
+        	$password = 'my+sql=i494f20_team12';
+        	$dbname = 'i494f20_team12';
+        	
+        	$conn = mysqli_connect($servername, $username, $password, $dbname);
+        	
+        	
+        	if ($conn->connect_error) {
+  			die("Connection failed: " . $conn->connect_error);
+                }
+                
+                $user = $_REQUEST['user'];
+                $event = $_REQUEST['event_name'];
+                $date = $_REQUEST
+                
+                $sql = "INSERT INTO Event (playlistName, email, Song ) VALUES ('$playlist','$user', '$song_array')";
+            $result = $conn->query($sql);
+            
+            echo mysqli_error(); ?>
