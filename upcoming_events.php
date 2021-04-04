d5f5978aef4a411f124c3e339ed173e2456ced05
ehuberm <ehuberm@iu.edu>
2021-04-04 17:32:26 -0400

    finished live event feature

---
+++ upcoming_events.php
@@ -0,0 +1,33 @@
+<?php 
+
+$servername='db.luddy.indiana.edu';
+        	$username= 'i494f20_team12';
+        	$password = 'my+sql=i494f20_team12';
+        	$dbname = 'i494f20_team12';
+        	
+        	$conn = mysqli_connect($servername, $username, $password, $dbname);
+        	
+        	
+        	if ($conn->connect_error) {
+  			die("Connection failed: " . $conn->connect_error);
+  			
+  			}
+  
+  $sql= "SELECT * from Event as e JOIN User as u On u.userID=e.userID";
+
+          $result = $conn->query($sql);
+          $row = $result->fetch_assoc() ;
+  
+  echo "<h1>Upcoming Events</h1>";
+  
+  while ($row = $result->fetch_assoc()) {
+  
+  
+  
+  
+  
+  echo "<a href='view_event.php?event=". $row['eventID']."'>".$row['eventName']." ".$row['date']." ".$row['time']." ".$row['firstName']." ".$row['lastName']."<br>";
+  
+  
+  }
+  ?>
---
+++ view_event.php
@@ -0,0 +1,54 @@
+<?php
+
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
+
+
+
+            		$servername='db.luddy.indiana.edu';
+        	$username= 'i494f20_team12';
+        	$password = 'my+sql=i494f20_team12';
+        	$dbname = 'i494f20_team12';
+        	
+        	$conn = mysqli_connect($servername, $username, $password, $dbname);
+        	
+        	
+        	if ($conn->connect_error) {
+  			die("Connection failed: " . $conn->connect_error);
+                }
+                
+                $eventID = $_REQUEST['event'] ;
+        
+    $sql = "SELECT * FROM Event where eventID = '".$eventID."'";
+    
+  //  echo "[$sql]";
+        
+        $result = $conn->query($sql);
+        
+		$row = $result->fetch_assoc() ;
+		
+  	//		echo "[".$row['playlistID']."][".$row['playlistName']."][".$row['Song']."]" ;
+  	
+  	
+  	echo "Event: ".$row['eventName']." ".$row['date']." ".$row['time']."<hr>" ;
+  			       
+  
+  $date = date('Y-m-d') ;
+  $time = date("H:i:s") ;
+  
+  if (($date >= $row['date']) and ($time >= $row['time']) ) {
+  
+        
+  
+  echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/LYMJyNEdoCk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' ;
+  
+ 
+ }
+ 		
+      
+
+
+
+ 
+?>
+
