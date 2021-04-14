b7b1394c0da7ab00dc2941e9108e5ae1d68d0cdc
Nagy, Allison L <allnagy@indiana.edu>
2021-04-14 16:26:31 -0400

    Update bio.php

--- quiz/bio.php
+++ quiz/bio.php
@@ -1,34 +1,29 @@
 <html>
 <head>
-<link rel="stylesheet" href="allisonstyle.css">
-<link href='https://fonts.googleapis.com/css?family=Almarai' rel='stylesheet'>
+<title>Bio Redirect</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/reset.css">
+        <link rel="stylesheet" href="./css/style.css">
+
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;800&display=swap" rel="stylesheet">
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>Create Bio</title>
+
+<link rel="stylesheet" href="style.css">
+
+
+<link rel="stylesheet" href="style.css">
 </head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <style>
-.topnav {
-  overflow: hidden;
-  background-color: #333;
-}
 
-.topnav a {
-  float: left;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #f1faee;
-  color: black;
-}
-
-.topnav a.active {
-  background-color: #8D99AE;
-  color: white;
-}
 
 h1 {
   font-family: 'Brush Script MT', cursive;
@@ -36,10 +31,10 @@ h1 {
 }
 
 div.form {
-	border-style: outset;
-	border-color: #2B2D42;
-	background-color: #8D99AE;
-	border-width:5px;
+        border-style: outset;
+        border-color: #2B2D42;
+        background-color: #8D99AE;
+        border-width:5px;
     display: block;
     margin-left: auto;
     margin-right: auto;
@@ -52,40 +47,64 @@ div.form {
 }
 </style>
 
-<body>
-<div class="topnav">
-  <a href="./index.html">Home</a>
-  <a href="#,/profile.php">Profile</a>
-  <a href="./post-form.html">Make Post</a>
-  <a href="./search.html">Search</a>
-  <a href="./saveSong.php">Save Song</a>
-  <a href="playlist">Make Playlist</a>
-  <a class="active" href="./setting.html">Settings</a>
-</div>
+        <nav class="container-fluid">
+            <div class="row">
+              
+                <div class="col-12 col-sm-12 text-center">
+                    <i class="d-inline fa fa-home me-2" aria-hidden="true"></i>
+                    <a  href ="./group.html">
+                        <i class="fa fa-users" aria-hidden="true"></i>
 
-<h1>Social Sounds</h1>
+                    </a>
+                    <div class="d-inline dropdown">
+                        <a class="btn fa fa-cog" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                        </a>
 
+                        <ul class="dropdown-menu setting-dropdown" aria-labelledby="dropdownMenuLink">
+                            <li><a class="dropdown-item text-center " href="./setting.html">Setting</a></li>
+                        </ul>
+                    </div>
+                        <button onclick="location.href = 'post-form.html';" id="makePostButton" class="submit-button">Make
+                        Post</button>
+                         <button onclick="location.href = 'search.html';" id="myButton" class="float-left submit-button">Search Music
+                        </button>
+                    <button onclick="location.href = 'saveSong.php';" id="saveMusicButton" class="submit-button">Save
+                        Music</button>
+                        <button onclick="location.href = 'upcoming_events.php';" id="myButton" class="float-left submit-button">Upcoming Events
+                        </button>
+     <button on			click="location.href = 'bio.html';" id="myButton" class="float-left submit-button">Create Bio
+                        </button>
+                        <button onclick="location.href = 'profile.php';" id="myButton" class="float-left submit-button">My Profile
+                        </button>
+                </div>
+            </div>
+        </nav>
+
+        <div class="main-header text-center pt-3 pb-3">
+            <h2>Thank you for using Social Sounds.</h2>
+        </div>
+        
 <div
-      Thank you for using Social Sounds. </br>
-      Click <a href ="./profile.php"> here </a> to view your profile.
+     <p> Click <a href ="./bioLoopkup.html"> here </a> to view your bio.</p>
+    
+
 </div>
 
-<body>
-</html>
-
-
+  
 <?php
 //connecting to server
-$servername = "db.luddy.indiana.edu";
-$username = "i494f20_allnagy";
-$password = "my+sql=i494f20_allnagy";
+$servername = "db.luddy.indiana.edu"; 
+$username = "i494f20_team12";
+$password = "my+sql=i494f20_team12";
 
-$conn = mysqli_connect($servername, $username, $password, 'i494f20_allnagy');
+$conn = mysqli_connect($servername, $username, $password,
+'i494f20_team12');
 if ($conn-> connect_error) {
   die("Connection falied:". $conn-> connect_error);
 }
-
-
+    
+      
 $userName = mysqli_real_escape_string($conn, $_REQUEST['userName']);
 $hometown = mysqli_real_escape_string($conn, $_REQUEST['hometown']);
 $genre = mysqli_real_escape_string($conn, $_REQUEST['genre']);
@@ -95,13 +114,18 @@ $song = mysqli_real_escape_string($conn, $_REQUEST['song']);
 //insert query execution
 if(isset($_POST['save']))
 {
-  $sql = "INSERT INTO bio (userName, hometown, genre, artist, song) VALUES ('$userName','$hometown','$genre','$artist','$song')";
+  $sql = mysqli_real_escape_string("INSERT INTO bio (userName, hometown, genre, artist, song) VALUES
+('$userName','$hometown','$genre','$artist','$song')");
   if(mysqli_query($conn, $sql)){
     echo "<script>alert('Records inserted successfully.')</script>";
   } else{
-    echo "<script>alert('ERROR: Could not able to execute $sql.')</script>" . mysqli_error($conn);
+    echo "<script>alert('ERROR: Could not able to execute
+$sql.')</script>" . mysqli_error($conn);
   }
 }
 
 $conn-> close();
 ?>
+
+</body>
+</html>
