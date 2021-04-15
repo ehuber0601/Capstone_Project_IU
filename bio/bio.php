de770920aed37933233775399d32be492db1ed8f
Nagy, Allison L <allnagy@indiana.edu>
2021-04-14 21:54:27 -0400

    Create bio.php

---
+++ bio/bio.php
@@ -0,0 +1,123 @@
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/reset.css">
+        <link rel="stylesheet" href="./css/style.css">
+
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;800&display=swap" rel="stylesheet">
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>Bio Redirect</title>
+
+    </head>
+    
+    <style>
+.group-setting {
+  margin: auto;
+  padding: 10px;
+  border-radius: 20px;
+  border: none;
+  background-color: #ffffff;
+  box-shadow: 0px 0px 5px #00000085;
+  width: 40%;
+}
+
+
+</style>
+
+    <body>
+        <nav class="container-fluid">
+            <div class="row">
+              	<a href="./index.html">
+                <div class="col-12 col-sm-12 text-center">
+                    <i class="d-inline fa fa-home me-2" aria-hidden="true"></i>
+                    <a  href ="./group.html">
+                        <i class="fa fa-users" aria-hidden="true"></i>
+
+                    </a>
+                    <div class="d-inline dropdown">
+                        <a class="btn fa fa-cog" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                        </a>
+
+                        <ul class="dropdown-menu setting-dropdown" aria-labelledby="dropdownMenuLink">
+                            <li><a class="dropdown-item text-center " href="./setting.html">Setting</a></li>
+                        </ul>
+                    </div>
+                        <button onclick="location.href = 'post-form.html';" id="makePostButton" class="submit-button">Make
+                        Post</button>
+                        <button onclick="location.href = 'search.html';" id="searchMusicButton" class="submit-button">Search Music
+        			</button>
+                    <button onclick="location.href = 'saveSong.php';" id="saveMusicButton" class="submit-button">Save
+                        Music</button>
+                        <button onclick="location.href = 'upcoming_events.php';" id="myButton" class="float-left submit-button">Upcoming Events
+                        </button>
+                        <button onclick="location.href = 'bio.html';" id="CreateBioButton" class="submit-button">Create Bio</button>
+
+                        <button onclick="location.href = 'profile.php';" id="myButton" class="float-left submit-button">My Profile
+                        </button>
+                          <button onclick="location.href = 'paypal.html';" id="DonateButton" class="submit-button">Donate </button>
+                </div>
+            </div>
+        </nav>
+
+        <div class="main-header text-center pt-3 pb-3">
+            <h2>Bio Lookup</h2>
+        </div>
+
+        <div class="group-setting">
+<p> Enter your username to view your personal bio information</p>
+</br>
+<form action="bioResults.php" method="post">
+Search <input type="text" name="username"><br>
+<input type ="submit">   
+</form>
+</div>
+    
+</body>
+</html>
+        
+
+  
+<?php
+//connecting to server
+$servername = "db.luddy.indiana.edu"; 
+$username = "i494f20_team12";
+$password = "my+sql=i494f20_team12";
+
+$conn = mysqli_connect($servername, $username, $password,
+'i494f20_team12');
+if ($conn-> connect_error) {
+  die("Connection falied:". $conn-> connect_error);
+}
+    
+      
+$userName = mysqli_real_escape_string($conn, $_REQUEST['userName']);
+$hometown = mysqli_real_escape_string($conn, $_REQUEST['hometown']);
+$genre = mysqli_real_escape_string($conn, $_REQUEST['genre']);
+$artist = mysqli_real_escape_string($conn, $_REQUEST['artist']);
+$song = mysqli_real_escape_string($conn, $_REQUEST['song']);
+
+//insert query execution
+if(isset($_POST['save']))
+{
+  $sql = mysqli_real_escape_string("INSERT INTO bio (userName, hometown, genre, artist, song) VALUES
+('$userName','$hometown','$genre','$artist','$song')");
+  if(mysqli_query($conn, $sql)){
+    echo "<script>alert('Records inserted successfully.')</script>";
+  } else{
+    echo "<script>alert('ERROR: Could not able to execute
+$sql.')</script>" . mysqli_error($conn);
+  }
+}
+
+$conn-> close();
+?>
+
+</body>
+</html>
