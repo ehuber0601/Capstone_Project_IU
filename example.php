aa8bd30ef61ab28c204df27b4ed4617ebc17c0b4
Liford, Emily C <eliford@indiana.edu>
2021-04-15 01:34:48 -0400

    Update example.php

--- example.php
+++ example.php
@@ -17,7 +17,7 @@ $user_name = mysqli_real_escape_string($link, $_REQUEST['user_name']);
 $song = mysqli_real_escape_string($link, $_REQUEST['song']);
 
 // Attempt insert query execution
-$sql = "INSERT INTO saveSong (username, song) VALUES ('$user_name', '$song')";
+$sql = "INSERT INTO saveSong (userName, songID) VALUES ('$user_name', '$song')";
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
 } else{
@@ -27,3 +27,4 @@ if(mysqli_query($link, $sql)){
 // Close connection
 mysqli_close($link);
 ?>
+
