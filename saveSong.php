b769c10d06d697df53fc6c10a9b9eb287cad1f96
Liford, Emily C <eliford@indiana.edu>
2021-04-15 01:34:17 -0400

    Update saveSong.php

--- saveSong.php
+++ saveSong.php
@@ -2,30 +2,52 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/reset.css">
+        <link rel="stylesheet" href="./css/style.css">
+
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;800&display=swap" rel="stylesheet">
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <title>Save Song Form</title>
-<div class="main-header text-center pt-3 pb-3">
-          </div>
-         <button onclick="location.href = 'index.html';" id="myButton" class="float-right submit-button" >Go Back Home</button>
-          <form class="form-signin" id="post-form" action="" onsubmit="event.preventDefault();post();">
+
 
 
 <style>
 
-  html {
-    background-color: #b3b3b4;
-  }
+table {
+  margin: auto;
+  padding: 10px;
+  background-color: #ffffff;
+  box-shadow: 0px 0px 5px #00000085;
+  width: 80%;
+  text-align: center;
+  font-size: 25px;
+  border-radius: 25px;
+  padding: 20px;
+  height: 150px;
+  border-collapse:separate;
+  border: 1px;
+  border-radius:6px;
+  -moz-border-radius:6px;
+}
 
-  table {
-    border-collapse: collapse;
-    width: 100%;
-    color: #b3b3b4;
-    font-family: "Manrope", sans-serif;
-    font-size: 25px;
-    text-align: left;
-  }
+td, th {
+  border-left: 1px;
+  border-top: 1px;
+}
+
+
+
+
   th {
-    background-color: blueviolet;
+    background-color: #487eb0;
     color: white;
+    border-top: none;
   }
   tr:nth-child(even) {background-color: #f2f2f2;}
 
@@ -38,6 +60,42 @@
 
 </head>
 <body>
+
+  <nav class="container-fluid">
+              <div class="row">
+                	<a href="./index.html">
+                  <div class="col-12 col-sm-12 text-center">
+                      <i class="d-inline fa fa-home me-2" aria-hidden="true"></i>
+                      <a  href ="./group.html">
+                          <i class="fa fa-users" aria-hidden="true"></i>
+
+                      </a>
+                      <div class="d-inline dropdown">
+                          <a class="btn fa fa-cog" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown"
+                              aria-expanded="false">
+                          </a>
+
+                          <ul class="dropdown-menu setting-dropdown" aria-labelledby="dropdownMenuLink">
+                              <li><a class="dropdown-item text-center " href="./setting.html">Setting</a></li>
+                          </ul>
+                      </div>
+                          <button onclick="location.href = 'post-form.html';" id="makePostButton" class="submit-button">Make
+                          Post</button>
+                          <button onclick="location.href = 'search.html';" id="searchMusicButton" class="submit-button">Search Music
+          			</button>
+                      <button onclick="location.href = 'saveSong.php';" id="saveMusicButton" class="submit-button">Save
+                          Music</button>
+                          <button onclick="location.href = 'upcoming_events.php';" id="myButton" class="float-left submit-button">Upcoming Events
+                          </button>
+                          <button onclick="location.href = 'bio.html';" id="CreateBioButton" class="submit-button">Create Bio</button>
+
+                          <button onclick="location.href = 'profile.php';" id="myButton" class="float-left submit-button">My Profile
+                          </button>
+                            <button onclick="location.href = 'paypal.html';" id="DonateButton" class="submit-button">Donate </button>
+                  </div>
+              </div>
+          </nav>
+
 
   <table>
   <tr>
@@ -63,6 +121,8 @@
     </p>
 
     <input type="submit" value="Submit">
+
+
 </form>
 </body>
 <?php
