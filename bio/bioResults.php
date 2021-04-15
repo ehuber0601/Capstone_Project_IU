c1b5520a1e4999ab0b66ea604d9bf7964ea2b8ec
Nagy, Allison L <allnagy@indiana.edu>
2021-04-14 21:45:04 -0400

    Update bioResults.php

--- bio/bioResults.php
+++ bio/bioResults.php
@@ -1,47 +1,37 @@
-<!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Your Bio</title>
 
-<style>
-.topnav {
-  overflow: hidden;
-  background-color: #333;
-}
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/reset.css">
+        <link rel="stylesheet" href="./css/style.css">
 
-.topnav a {
-  float: left;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;800&display=swap" rel="stylesheet">
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>Your Bio</title>
 
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
+    </head>
 
-.topnav a.active {
-  background-color: #D8BFD8;
-  color: white;
-}
-
-
-
-  table {
+    <style>
+      table {
+      margin: auto;
+    padding: 15px;
     border-collapse: collapse;
-    width: 100%;
+    width: 60%;
     color: #b3b3b4;
     font-family: "Manrope", sans-serif;
     font-size: 25px;
     text-align: left;
+    box-shadow: 0px 0px 5px #00000085;
   }
   th {
-    background-color: navy;
+    background-color: #353b48;
     color: white;
+    padding: 15px;
   }
   tr:nth-child(even) {background-color: #f2f2f2;}
 
@@ -49,61 +39,47 @@
 
 
 </style>
-	
 
+    <body>
+        <nav class="container-fluid">
+            <div class="row">
+              	<a href="./index.html">
+                <div class="col-12 col-sm-12 text-center">
+                    <i class="d-inline fa fa-home me-2" aria-hidden="true"></i>
+                    <a  href ="./group.html">
+                        <i class="fa fa-users" aria-hidden="true"></i>
 
+                    </a>
+                    <div class="d-inline dropdown">
+                        <a class="btn fa fa-cog" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                        </a>
 
-  
-  
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width,
-initial-scale=1.0">
+                        <ul class="dropdown-menu setting-dropdown" aria-labelledby="dropdownMenuLink">
+                            <li><a class="dropdown-item text-center " href="./setting.html">Setting</a></li>
+                        </ul>
+                    </div>
+                        <button onclick="location.href = 'post-form.html';" id="makePostButton" class="submit-button">Make
+                        Post</button>
+                        <button onclick="location.href = 'search.html';" id="searchMusicButton" class="submit-button">Search Music
+        			</button>
+                    <button onclick="location.href = 'saveSong.php';" id="saveMusicButton" class="submit-button">Save
+                        Music</button>
+                        <button onclick="location.href = 'upcoming_events.php';" id="myButton" class="float-left submit-button">Upcoming Events
+                        </button>
+                        <button onclick="location.href = 'bio.html';" id="CreateBioButton" class="submit-button">Create Bio</button>
 
-        <link rel="stylesheet" href="./css/reset.css">
-        <link rel="stylesheet" href="./css/style.css">
-    
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link
-href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;800&display=swap"
-rel="stylesheet">
-        <link rel="stylesheet"
-  
-href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <title>Home Page</title>
-   
-        <script> let session_id = localStorage.getItem("session_id");
+                        <button onclick="location.href = 'profile.php';" id="myButton" class="float-left submit-button">My Profile
+                        </button>
+                          <button onclick="location.href = 'paypal.html';" id="DonateButton" class="submit-button">Donate </button>
+                </div>
+            </div>
+        </nav>
 
-  
-            if (session_id === "" || session_id === null) {
-                location.href = "./login.html"
-                         location.href = "./login.html"
-            }
-        </script>
-        <script>
-  
-        
-</script>
-<style>
-.profile { display: block;
-float: left;}
-.profilepage {font-size: 20px;
-color: black; }
-</style>
-    </head>  
+        <div class="main-header text-center pt-3 pb-3">
+            <h2>Your Bio Information</h2>
+        </div>
 
-<div class="topnav">
-  <a href="./index.html">Home</a>
-  <a href="./post-form.html">Make Post</a>
-  <a href="./saveSong.php">Save Music</a>
-  <a class="active" href="upcoming_events.php">Events</a>
-  <a href="./bio.html">Create Bio</a>
-  <a href="./profile.php">My Profile</a>
-  <a href="./teammembers.html">Team Members</a>
-  <a href="./setting.html">Settings</a>
-</div
-                
-                         
-<body>
         
   <table>
   <tr>  
@@ -138,8 +114,11 @@ if ($result-> num_rows > 0) {
   echo "</table>";
 }
 else {
-  echo "0 result";
+  echo "No bio found for this user";
 }
 
 $conn-> close();
 ?>
+
+</body>
+</html>
