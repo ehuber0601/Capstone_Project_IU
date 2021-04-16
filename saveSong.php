b2465cf60a926093849179ffc620f870d591ea29
Nagy, Allison L <allnagy@indiana.edu>
2021-04-16 16:55:11 -0400

    Update saveSong.php
    added add song navigation

--- saveSong.php
+++ saveSong.php
@@ -15,6 +15,46 @@
 
     </head>
 
+
+
+    <style>
+      table {
+      margin: auto;
+    padding: 15px;
+    border-collapse: collapse;
+    width: 60%;
+    color: #b3b3b4;
+    font-family: "Manrope", sans-serif;
+    font-size: 25px;
+    text-align: left;
+    box-shadow: 0px 0px 5px #00000085;
+  }
+  th {
+    background-color: #353b48;
+    color: white;
+    padding: 15px;
+
+  }
+  tr:nth-child(even) {background-color: #f2f2f2;}
+
+  tr:nth-child(odd) {background-color: white;}
+  
+      <style>
+.group-setting {
+  padding: 10px;
+  border-radius: 20px;
+  border: none;
+  background-color: #ffffff;
+  box-shadow: 0px 0px 5px #00000085;
+  width: 40%;
+  font-size:15px;
+  text-align: center;
+}
+
+
+</style>
+
+
     <body>
         <nav class="container-fluid">
             <div class="row">
@@ -55,100 +95,12 @@
             <h2>Save Music</h2>
         </div>
 
-    <style>
-      table {
-      margin: auto;
-    padding: 15px;
-    border-collapse: collapse;
-    width: 60%;
-    color: #b3b3b4;
-    font-family: "Manrope", sans-serif;
-    font-size: 25px;
-    text-align: left;
-    box-shadow: 0px 0px 5px #00000085;
-  }
-  th {
-    background-color: #353b48;
-    color: white;
-    padding: 15px;
 
-  }
-  tr:nth-child(even) {background-color: #f2f2f2;}
-
-  tr:nth-child(odd) {background-color: white;}
-  
-      <style>
-.group-setting {
-  padding: 10px;
-  border-radius: 20px;
-  border: none;
-  background-color: #ffffff;
-  box-shadow: 0px 0px 5px #00000085;
-  width: 40%;
-  font-size:15px;
-  text-align: center;
-}
-
-
-</style>
-
-
-
-</head>
-<body>
-
-  <nav class="container-fluid">
-              <div class="row">
-                	<a href="./index.html">
-                  <div class="col-12 col-sm-12 text-center">
-                      <i class="d-inline fa fa-home me-2" 
-aria-hidden="true"></i>
-                      <a  href ="./group.html">
-                          <i class="fa fa-users" aria-hidden="true"></i>
-
-                      </a>
-                      <div class="d-inline dropdown">
-                          <a class="btn fa fa-cog" href="#" role="button" 
-id="dropdownMenuLink" data-bs-toggle="dropdown"
-                              aria-expanded="false">
-                          </a>
-
-                          <ul class="dropdown-menu setting-dropdown" 
-aria-labelledby="dropdownMenuLink">
-                              <li><a class="dropdown-item text-center " 
-href="./setting.html">Setting</a></li>
-                          </ul>
-                      </div>
-                          <button onclick="location.href = 
-'post-form.html';" id="makePostButton" class="submit-button">Make
-                          Post</button>
-                          <button onclick="location.href = 'search.html';" 
-id="searchMusicButton" class="submit-button">Search Music
-          			</button>
-                      <button onclick="location.href = 'saveSong.php';" 
-id="saveMusicButton" class="submit-button">Save
-                          Music</button>
-                          <button onclick="location.href = 
-'upcoming_events.php';" id="myButton" class="float-left 
-submit-button">Upcoming Events
-                          </button>
-                          <button onclick="location.href = 'bio.html';" 
-id="CreateBioButton" class="submit-button">Create Bio</button>
-
-                          <button onclick="location.href = 'profile.php';" 
-id="myButton" class="float-left submit-button">My Profile
-                          </button>
-                            <button onclick="location.href = 
-'paypal.html';" id="DonateButton" class="submit-button">Donate </button>
-                  </div>
-              </div>
-          </nav>
 
 
   <table>
   <tr>
     <th>Song ID</th>
-    <th>Artist ID</th>
     <th>Artist Name</th>
     <th>Length</th>
     <th>Genre</th>
@@ -175,6 +127,8 @@ id="myButton" class="float-left submit-button">My Profile
 
 
 </form>
+<p></p>
+<p> Don't see the song you're looking for? Click <a href="./addSong.html"> here </a> to add it to our song library. </p>
 </div>
 </body>
 <?php
@@ -190,14 +144,13 @@ if ($conn-> connect_error) {
 }
 
 //showing table of songs
-$sql = "SELECT songID, artistID, artistName, length, genre, title FROM 
+$sql = "SELECT songID, artistName, length, genre, title FROM 
 Song";
 $result = $conn-> query($sql);
 
 if ($result-> num_rows > 0) {
   while ($row = $result-> fetch_assoc()) {
-    echo "<tr><td>". $row["songID"] ."</td><td>". $row["artistID"] 
-."</td><td>". $row["artistName"] ."</td><td>". $row["length"] 
+    echo "<tr><td>". $row["songID"] ."</td><td>". $row["artistName"] ."</td><td>". $row["length"] 
 ."</td><td>". $row["genre"] ."</td><td>". $row["title"] ."</td></tr>";
   }
   echo "</table>";
@@ -213,4 +166,3 @@ $conn-> close();
 
 
 </html>
-
